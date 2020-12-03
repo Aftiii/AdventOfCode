77b4dc9ba80eb5d634f499a49d14dbbffcedbc46
Robbie Aftab <raftab@trakglobal.co.uk>
2020-12-03 23:48:05 +0000

    day 3, annoying

--- AdventOfCode/Logic/Day_03/Day_03.cs
+++ AdventOfCode/Logic/Day_03/Day_03.cs
@@ -6,17 +6,53 @@ namespace AdventOfCode
     public class Day_03 : BaseDay
     {
         private readonly string _input;
-
+        private readonly string[] lines;
         public Day_03()
         {
             if (File.Exists(InputFilePath))
             {
                 _input = File.ReadAllText(InputFilePath);
+                lines = _input.Split("\r\n");
             }
         }
+        public override string Solve_1()
+        {
+            return $"{CountTrees(3,1)}";
+        }
 
-        public override string Solve_1() => $"Not implemented.. yet";
+        public override string Solve_2()
+        {
+            long first = CountTrees(1, 1);
+            long second = CountTrees(3, 1);
+            long third = CountTrees(5, 1);
+            long fourth = CountTrees(7, 1);
+            long fifth = CountTrees(1, 2);
 
-        public override string Solve_2() => $"Not implemented.. yet";
+            long total = first * second * third * fourth * fifth;
+            return $"{total}";
+        }
+        private int CountTrees(int right, int down)
+        {
+            int currentX = 0;
+            int trees = 0;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i][currentX].ToString() == "#")
+                {
+                    trees++;
+                }
+                currentX += right;
+
+                if (currentX >= lines[i].Length)
+                {
+                    currentX -= lines[i].Length;
+                }
+                if (down > 1)
+                {
+                    i += (down - 1);
+                }
+            }
+            return trees;
+        }
     }
 }
